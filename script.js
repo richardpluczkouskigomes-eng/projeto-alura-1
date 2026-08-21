06e270b772192be6d297931874b51cde1fc0b563
richardpluczkouskigomes-eng <richard.pluczkouski.gomes@escola.pr.gov.br>
2026-08-21 08:31:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ botoes.forEach(function (botao) {
         }
     }
 });
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+    function mudaTema() {
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
+}
